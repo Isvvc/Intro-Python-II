b9aca9a4f069e7f512a86a79c1d0ddc2f7b924db
Isvvc <lyons@tuta.io>
2020-03-12 10:55:51 -0600

    Add items to rooms

--- src/adv.py
+++ src/adv.py
@@ -1,18 +1,23 @@
 from room import Room
 from player import Player
+from item import Item
 
 # Declare all the rooms
+
+jug = Item("jug", "a clay pitcher")
+pick = Item("pickaxe", "a stone pickaxe")
+rock = Item("rock", "just a rock")
 
 room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", [jug]),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", [pick, rock]),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
@@ -63,7 +68,10 @@ while True:
         break
     elif cmd in ["n", "s", "e", "w"]:
         if player.move(cmd):
-            print(player.current_room.name)
-            print(player.current_room.description)
+            room = player.current_room
+            print(room.name)
+            print(room.description)
+            if room.list_items() is not None:
+                print(room.list_items())
         else:
             print("You can't go that way.")
--- src/room.py
+++ src/room.py
@@ -1,14 +1,17 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 
+from item import Item
+
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, items = []):
         self.name = name
         self.description = description
         self.n_to = None
         self.s_to = None
         self.e_to = None
         self.w_to = None
+        self.items = items
     def room_in_direction(self, direction):
         directions = {
             "n": self.n_to,
@@ -17,3 +20,12 @@ class Room:
             "w": self.w_to
         }
         return directions[direction]
+    def list_items(self):
+        if len(self.items) > 0:
+            output = "In the room, you see: "
+            for item in self.items:
+                if isinstance(item, Item):
+                    output += f"{item.name}, "
+            return output[:-2]
+        else:
+            return None
